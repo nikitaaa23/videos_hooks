da40ff00163ec3f64ffc32c0b5a16a2079e9c933
Nikita <saininikita23@gmail.com>
2022-01-07 17:46:23 +0530

    commit

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import VideoItem from './VideoItem';
+import React from "react";
+import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
-  const renderedList = videos.map((video) => {
-    return <VideoItem video={video} />;
-  });
-
-  return <div>{renderedList}</div>;
+    const renderedList = videos.map((video) => {
+        return <VideoItem video = {video} />;
+    });
+    return <div>{renderedList}</div>;
 };
+
 
 export default VideoList;
