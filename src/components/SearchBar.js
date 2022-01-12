a95d7170b41ad68983a33b312ac6bcb205f40cf8
Nikita <saininikita23@gmail.com>
2022-01-12 17:13:03 +0530

    commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,15 @@
 import React, {useState} from 'react';
 
 
-<<<<<<< HEAD
-const SearchBar = ({onFormSubmit}) => {
+const SearchBar = ({ onFormSubmit} ) => {
   const [term, setTerm] = useState('');
-
-  const onInputChange = (event) => {
-    setTerm(event.target.value );
-=======
-const SearchBar = ( onFormSubmit ) => {
-  const [term, setTerm] = useState('');
-
-  const onInputChange = (event) => {
-    setTerm(event.target.value);
->>>>>>> 76500dfc68d9c27e03ab72044678cf6caaae7629
-  };
 
   const onSubmit = (event) => {
     event.preventDefault();
-
     onFormSubmit(term);
-<<<<<<< HEAD
-
-    return (
-=======
   };
 
-
 return (
->>>>>>> 76500dfc68d9c27e03ab72044678cf6caaae7629
       <div className="search-bar ui segment">
         <form onSubmit={onSubmit} className="ui form">
           <div className="field">
@@ -36,23 +17,13 @@ return (
             <input
               type="text"
               value={term}
-<<<<<<< HEAD
-              onChange={(event)=> setTerm(event.target.value)}
-=======
               onChange={(event) => setTerm(event.target.value)}
->>>>>>> 76500dfc68d9c27e03ab72044678cf6caaae7629
             />
           </div>
         </form>
       </div>
     );
-<<<<<<< HEAD
-
   };
-};
 
-=======
-};
->>>>>>> 76500dfc68d9c27e03ab72044678cf6caaae7629
 
 export default SearchBar;
